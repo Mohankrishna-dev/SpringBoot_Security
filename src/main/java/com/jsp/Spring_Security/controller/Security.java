2185adb54152b6e5b6c6c75f37959b2ac6d63d82
Mohankrishna-dev <mohankrishnak742@gmail.com>
2026-06-10 17:15:10 +0530

    First commit: Initial project upload

---
+++ src/main/java/com/jsp/Spring_Security/controller/Security.java
@@ -0,0 +1,21 @@
+package com.jsp.Spring_Security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Security {
+@GetMapping("/")
+	public String msg() {
+		return "Spring Security classs";
+	}
+@GetMapping("/public")
+public String user1() {
+	return "this is public";
+}
+@GetMapping("/private")
+public String hello() {
+	return "this is private";
+}
+
+}
